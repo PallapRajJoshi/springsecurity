94468cadae504e2751bf1b14a2b4042b95782dac
PallapRajJoshi <pallaprajjoshi@gmail.com>
2024-11-10 08:51:09 +0545

    user name and password updated

--- src/main/java/com/blog/app/controller/HelloController.java
+++ src/main/java/com/blog/app/controller/HelloController.java
@@ -3,11 +3,13 @@ package com.blog.app.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 public class HelloController {
 	
 @GetMapping("/")
-public String greet() {
-	return "Welcome Pallap";
+public String greet(HttpServletRequest request) {
+	return "Welcome Pallap"+request.getSession().getId();
 }
 }
